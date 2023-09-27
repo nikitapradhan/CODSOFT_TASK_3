6510e2c4f201f679447c2d8a9baeb7effa8f5968
nikitapradhan <143255998+nikitapradhan@users.noreply.github.com>
2023-09-27 21:49:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+let displayValue = '0';
+
+function updateDisplay() {
+    const display = document.getElementById('display');
+    display.value = displayValue;
+}
+
+function appendOperator(operator) {
+    const operators = ['+', '-', '*', '/'];
+
+    if (operators.includes(displayValue.slice(-1))) {
+        displayValue = displayValue.slice(0, -1) + operator;
+    } else {
+        displayValue += operator;
+    }
+
+    updateDisplay();
+}
+
+function appendNumber(number) {
+    displayValue += number;
+    updateDisplay();
+}
+
+function appendDecimal(decimal) {
+    if (!displayValue.includes(decimal)) {
+        displayValue += decimal;
+        updateDisplay();
+    }
+}
+
+function backspace() {
+    displayValue = displayValue.slice(0, -1);
+    updateDisplay();
+}
+
+function clearDisplay() {
+    displayValue = '';
+    updateDisplay();
+}
+
+function calculate() {
+    try {
+        const result = eval(displayValue);
+        displayValue = String(result);
+        updateDisplay();
+    } catch (error) {
+        displayValue = '';
+        updateDisplay();
+    }
+}
